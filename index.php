370bb685446714696511216a45c5ae39086eb3e4
windrushfarer <definiti@mail.ru>
2016-09-27 17:54:37 +0500

    new site

---
+++ index.php
@@ -0,0 +1,159 @@
+﻿<?php include 'header.php' ?>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner" id="about">
+                <h1>Welcome!</h1>
+                <hr>
+                <p>Я - Огарков Егор, веб-программист. Разрабатываю веб-сайты,
+                    верстаю Front-end, пишу приложения на JavaScript</p>
+                <a href="#info" class="btn btn-primary btn-xl page-scroll">
+                    Узнать больше</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="info">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Вы сделаете правильный выбор!</h2>
+                    <hr class="light">
+                    <p class="text-faded">Вам будут предоставленны качественные услуги, а работа
+                    сделана в срок.<br>
+                        Ниже вы можете ознакомится с проектами, в которых я принимал
+                    участие или сам разрабатывал. Надеюсь вам понравится!</p>
+                    <a href="#portfolio" class="btn btn-default btn-xl page-scroll">Посмотреть!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Почему стоит выбрать?</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
+                        <h3>Чистая работа</h3>
+                        <p class="text-muted">Ваш заказ будет выполнен в чистом в виде,
+                            ничего лишнего.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                        <h3>Полная готовность</h3>
+                        <p class="text-muted">Есть идея? Напишите и вам в кратчайшее время ответят,
+                        чтобы решить вашу задачу.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <h3>Работа в срок</h3>
+                        <p class="text-muted">Заказ выполняется строго по графику, нарушения
+                        дедлайнов крайне редки</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                        <h3>Сделано с любовью</h3>
+                        <p class="text-muted">В каждый проект разработчик вкладывает частичку своей души!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>Портфолио</h2>
+            </div>
+        </div>
+    </aside>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="assets/img/portfolio/1.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Здесь может быть<!-- Категория -->
+                                </div>
+                                <div class="project-name">
+                                    Ваш проект <!-- Название проекта -->
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="assets/img/portfolio/2.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Здесь может быть <!-- Категория -->
+                                </div>
+                                <div class="project-name">
+                                    Ваш проект <!-- Название проекта -->
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="assets/img/portfolio/3.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Здесь может быть <!-- Категория -->
+                                </div>
+                                <div class="project-name">
+                                    Ваш проект <!-- Название проекта -->
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Свяжитесь со мной</h2>
+                    <hr class="primary">
+                    <p>Уже готовы реализовать Ваш проект? Тогда напишите мне и вы получите ответ так скоро, как это возможно. 
+                    </p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-skype fa-3x wow bounceIn"></i>
+                    <p>blackwindrush</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                    <p><a href="mailto:windrushfarer@gmail.com">windrushfarer@gmail.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php include('footer.php'); ?>
